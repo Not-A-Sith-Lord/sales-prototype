49e7663558db6dee605cd9d8a5843bfa99768223
Not-A-Sith-Lord <arieldeschapell@gmail.com>
2017-06-27 16:10:03 -0400

    have leads display working on user dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,11 +3,40 @@ const router  = express.Router();
 const passport = require("passport");
 const Lead = require("../models/lead.js");
 const User = require("../models/user.js");
+const mongoose = require("mongoose");
 
 /* GET DASHBOARD */
 
 router.get('/', checkRoles('USER'),(req, res) => {
-  res.render('dash', {user: req.user, email: req.user.email});
+
+
+  //If the user has no leads skip all these calls
+  if (req.user.leads){
+
+  const queryArray = [];
+  //Loop through list of leads IDs and build query array
+  (req.user.leads).forEach((lead) => {
+    queryArray.push(mongoose.Types.ObjectId(lead))
+  });
+
+  //Search with query array
+  Lead.find({
+    '_id': { $in: queryArray}
+}, (err, docs) => {
+  //Load dash with lead docs
+     res.render('dash', {
+       user: req.user,
+       email: req.user.email,
+       leads : docs
+     });
+});
+
+} //End first conditional
+  else {
+     res.render('dash', {user: req.user, email: req.user.email});
+}
+
+
 });
 
 
@@ -17,14 +46,7 @@ router.get("/create-lead", checkRoles('USER'),(req, res, next)=> {
 });
 
 router.post("/create-lead", (req, res, next)=> {
-  const firstName = req.body.firstName;
-  const lastName  = req.body.lastName;
-  const email     = req.body.email;
-  const phone     = req.body.phone;
-  const zip       = req.body.zip;
-  const address   = req.body.address;
-  const city      = req.body.city;
-  const company   = req.body.company;
+
   //Gotta slice keywords into array
   const keywords  = req.body.keywords;
 console.log("testing create lead route");
@@ -38,14 +60,14 @@ console.log("testing create lead route");
     }
 
     const newLead = Lead({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      phone: phone,
-      zip: zip,
-      address: address,
-      city: city,
-      company: company,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      zip: req.body.zip,
+      address: req.body.address,
+      city: req.body.city,
+      company: req.body.company,
       keywords: ["test", "another test", "yet another test"]
     });
   //New lead is created with a _id here. Now we'll 1. save to DB and
@@ -55,12 +77,7 @@ console.log("testing create lead route");
       if (err) {
         res.render("auth/signup", { message: "Something went wrong" });
       } else {
-        console.log("/////////////////////////////////////////////////////////////////////////");
-        console.log(req.user.leads);
-        console.log(newLead._id);
-        // const array = req.user.leads;
-        // const newLeads = array.push(newLead._id);
-        console.log(newLead);
+
         //Save newLead id to user leads array
         User.findByIdAndUpdate(req.user._id, {
           "$push" : {leads: newLead._id},
@@ -70,17 +87,17 @@ console.log("testing create lead route");
              console.log("Lead saved but not attached to user");
            }
            else {
+               //Redirect to dash
              res.redirect("/dashboard");
            }
-})
-
-        //Redirect to dash
-
+         });
+         //End find and update
       }
+      //End else if no save erorr
     });
-
+    //End Save
 });
-
+//End Find One
 });
 //End of Create Lead
 
